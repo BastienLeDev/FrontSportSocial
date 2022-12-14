182bcb7b26583acd281b4a02182c7303c676c781
HugoLePetitDev <hugopineau98@gmail.com>
2022-12-14 14:06:33 +0100

    messagerie

--- frontSportSocial/src/app/friend/friend.component.ts
+++ frontSportSocial/src/app/friend/friend.component.ts
@@ -1,6 +1,7 @@
 import { AnimateTimings } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-friend',
@@ -10,19 +11,24 @@ import { Component, OnInit } from '@angular/core';
 export class FriendComponent implements OnInit {
 
   messages : any;
+  id : any;
 
 
-  constructor (private http : HttpClient) {}
+  constructor (private http : HttpClient, public authService: AuthService) {}
 
   ngOnInit() : void {
-    this.http.get('http://localhost:8300/message/me/6').subscribe({
-      next: (data) => {this.messages = data},
-      error: (err) => {console.log(err)}
-    })
+    this.listMessages();
+
   }
 
+  listMessages(){
 
+  this.http.get('http://localhost:8300/message/me/' + this.authService.getUserConnect().idUser).subscribe({
+    next: (data) => {this.messages = data},
+    error: (err) => {console.log(err)}
+  });
 
 }
 
+}
 
