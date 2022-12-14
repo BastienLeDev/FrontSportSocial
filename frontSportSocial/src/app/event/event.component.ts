91b52c6b5bcf4336a3a12b7db98b5ded4135454c
LelucFromEsic <leluc@etudiant-esic.fr>
2022-12-14 15:06:02 +0100

    lien Api event

--- frontSportSocial/src/app/event/event.component.ts
+++ frontSportSocial/src/app/event/event.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css']
 })
-export class EventComponent {
+export class EventComponent implements OnInit{
+
+  events : any;
+
+  constructor(private http: HttpClient, private route: Router) { }
+
+  ngOnInit(): void {
+    this.listEventToCome();
+  }
+
+
+
+  createEvent(val: any) {
+    this.http.post('http://localhost:8300/event/create', val).subscribe({})
+
+  }
+
+  listEventToCome(){
+    this.http.get('http://localhost:8300/event/tocome').subscribe({
+      next: (data) => { this.events = data },
+      error: (err) => { console.log(err); }
+    });
+  }
 
 }
