f90065b51c0edabfae4466615559c3999313df41
LucyDdt <dolucy@etudiant-esic.fr>
2022-12-14 11:46:37 +0100

    inscription qui marche (pas d'adresse qd inscrire)

--- frontSportSocial/src/app/inscription/inscription.component.ts
+++ frontSportSocial/src/app/inscription/inscription.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class InscriptionComponent {
 
-
+  user: any;
 
   constructor(private http: HttpClient, private route: Router) { }
 
@@ -18,7 +18,15 @@ export class InscriptionComponent {
   }
 
   inscription(val: any) {
-    this.http.post('http://localhost:8300/user/save', val).subscribe({})
+    this.http.post('http://localhost:8300/user/save', val).subscribe({
+      next: (data) => {
+        this.user = data;
+        console.log(this.user);
+        this.route.navigateByUrl('connexion');
+      },
+      error: (err) => { console.log(err) },
+
+    })
 
   }
 
