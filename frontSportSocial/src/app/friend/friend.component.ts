a47d19ecbebd30895ffea2722c618a2778cd0fc3
HugoLePetitDev <hugopineau98@gmail.com>
2022-12-14 12:39:34 +0100

    Friend change

--- frontSportSocial/src/app/friend/friend.component.ts
+++ frontSportSocial/src/app/friend/friend.component.ts
@@ -5,17 +5,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-friend',
   templateUrl: './friend.component.html',
-  styleUrls: ['./friend.component.css']
+  styleUrls: ['./friend.component.css'],
 })
 export class FriendComponent implements OnInit {
+
+  messages : any;
 
 
   constructor (private http : HttpClient) {}
 
   ngOnInit() : void {
-
+    this.http.get('http://localhost:8300/message/me/6').subscribe({
+      next: (data) => {this.messages = data},
+      error: (err) => {console.log(err)}
+    })
   }
 
 
 
 }
+
+
