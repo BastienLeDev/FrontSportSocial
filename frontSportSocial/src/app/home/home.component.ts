8f1bf8b76e821dda59af92a8c4cad366b5317e3c
JeremyH1998 <jhenriongp@gmail.com>
2022-12-14 12:37:08 +0100

    affichage données et liste amis dans home

--- frontSportSocial/src/app/home/home.component.ts
+++ frontSportSocial/src/app/home/home.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  friends: any;
+  id: any;
+
+  constructor(private http: HttpClient, public authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.listFriends();
+
+  }
+
+
+  listFriends() {
+
+
+
+    this.http.get('http://localhost:8300/friend/receiver/' + this.authService.getUserConnect().idUser).subscribe({
+      next: (data) => { this.friends = data },
+      error: (err) => { console.log(err); }
+
+    });
+  }
 
 }
