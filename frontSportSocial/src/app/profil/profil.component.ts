b9990f60429630d12aa597aa6ec068996438288e
LucyDdt <dolucy@etudiant-esic.fr>
2022-12-14 14:59:13 +0100

    modifs du profil, ajout boutons

--- frontSportSocial/src/app/profil/profil.component.ts
+++ frontSportSocial/src/app/profil/profil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-profil',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./profil.component.css']
 })
 export class ProfilComponent {
+
+  constructor(public authService: AuthService) { }
 
 }
