fe7235350932a643d67b7b391a8dde15d119ee72
LelucFromEsic <leluc@etudiant-esic.fr>
2022-12-15 12:53:17 +0100

    creation event via form

--- frontSportSocial/src/app/event/event.component.ts
+++ frontSportSocial/src/app/event/event.component.ts
@@ -11,29 +11,34 @@ export class EventComponent implements OnInit{
 
   events : any;
   event : any;
+  sports : any;
 
   constructor(private http: HttpClient, private route: Router) { }
 
   ngOnInit(): void {
     this.listEventToCome();
+    this.listSport();
   }
 
 
 
   createEvent(val: any) {
+    console.log(val)
     this.http.post('http://localhost:8300/event/create', val).subscribe({
-      next:(data)=>{
-        this.event = data;
-        console.log(this.event);
-      },
       error: (err) => { console.log(err) },
-    })
-
+    });
   }
 
   listEventToCome(){
     this.http.get('http://localhost:8300/event/tocome').subscribe({
       next: (data) => { this.events = data },
+      error: (err) => { console.log(err); }
+    });
+  }
+
+  listSport(){
+    this.http.get('http://localhost:8300/sport').subscribe({
+      next: (data) => { this.sports = data },
       error: (err) => { console.log(err); }
     });
   }
