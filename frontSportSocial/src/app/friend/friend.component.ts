ef0695ee0a56685dac561ae2cd75446dffdd9926
HugoLePetitDev <hugopineau98@gmail.com>
2022-12-23 10:17:15 +0100

    MAJ parceque Raph a fait n'importe quoi

--- frontSportSocial/src/app/friend/friend.component.ts
+++ frontSportSocial/src/app/friend/friend.component.ts
@@ -174,6 +174,7 @@ export class FriendComponent implements OnInit {
     this.http.post('http://localhost:8300/friend/' + this.authService.getUserConnect().idUser, this.login3).subscribe({
       next: (data) => {
         console.log(this.login3)
+        this.ngOnInit();
       }
     })
 
