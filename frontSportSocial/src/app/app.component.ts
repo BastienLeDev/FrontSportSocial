2d78a0be00d116708ef1bb2dcb7739b587b15b15
HugoLePetitDev <hugopineau98@gmail.com>
2022-12-26 17:58:24 +0100

    dark theme 1.0

--- frontSportSocial/src/app/app.component.ts
+++ frontSportSocial/src/app/app.component.ts
@@ -7,8 +7,29 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.css'],
 
 })
+
+
 export class AppComponent {
   title = 'frontSportSocial';
+  dark = false;  
 
   constructor(public authService: AuthService) { };
+
+  toggle = true;
+  status = 'Enable'; 
+
+
+enableDisableRule() {
+    this.toggle = !this.toggle;
+    this.status = this.toggle ? '' : '';
+}
+
+darkMode() {
+  if (this.dark == false) {
+    this.dark = true;
+  } else {
+    this.dark = false;
+  }
+}
+
 }
