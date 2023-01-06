601786e83909124355b7216ffd31125f343a2644
BastienLeDev <gauthiero.bastien@outlook.com>
2023-01-06 12:04:21 +0100

    pop up

---
+++ frontSportSocial/src/app/pop-up-coach/pop-up-coach.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up-coach',
+  templateUrl: './pop-up-coach.component.html',
+  styleUrls: ['./pop-up-coach.component.css']
+})
+export class PopUpCoachComponent {
+
+}
